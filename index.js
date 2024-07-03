e189b7e2717873cce9424ebd50ef33ff0a4f2000
AlexandreHSSDerick <108272270+AlexandreHSSDerick@users.noreply.github.com>
2024-07-03 12:33:06 +0000

    Criação da lista e resposta HTTP

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Desafio: Criar um endpoint /oi
 app.get('/oi', function (req, res){
   res.send('Olá, Mundo!')
+})
+
+//lista de personagens
+
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith' ]
+
+//READ All - [GET] /item
+app.get('/item', function (req, res){
+  //Pegamos a lista e enviamos como resposta HTTP
+  res.send(lista)
 })
 
 app.listen(3000)
